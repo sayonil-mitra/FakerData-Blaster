fbcfba583ff905dbc08375cd38985d6ca3bbb83f
TSAGAR1999 <153739344+TSAGAR1999@users.noreply.github.com>
2024-09-06 02:05:33 +0530

    Created helper function to push data for AegisApp

--- src/aegisAppData/helpers.js
+++ src/aegisAppData/helpers.js
@@ -1,11 +1,16 @@
 import { faker } from "../faker/faker.js";
-
+import { camera_imageurls, camera_videourls } from "./cam_images_videos.js";
 
 const createCameraData = (purgeId) => ({
   camera_id: faker.string.uuid(),
-  camera_videourl: faker.internet.url(),
-  camera_imageurl: faker.image.imageUrl(),
-  camera_type: faker.helpers.arrayElement(["CCTV", "IP Camera", "Webcam"]),
+  camera_videourl: faker.helpers.arrayElement(camera_videourls),
+  camera_imageurl: faker.helpers.arrayElement(camera_imageurls),
+  camera_type: faker.helpers.arrayElement([
+    "body",
+    "drone",
+    "Surveillance",
+    "Street",
+  ]),
   camera_pincode: faker.location.zipCode(),
   camera_area: faker.location.streetName(),
   camera_pinned: faker.datatype.boolean(),
@@ -13,15 +18,7 @@ const createCameraData = (purgeId) => ({
     latitude: faker.location.latitude(),
     longitude: faker.location.longitude(),
   },
-  language: faker.helpers.arrayElement([
-    "en",
-    "hi",
-    "te",
-    "ml",
-    "kn",
-    "gu",
-    "ta",
-  ]),
+  language: faker.helpers.arrayElement(["en"]),
   cityname: faker.location.city(),
   purge_id: purgeId,
 });
@@ -116,12 +113,108 @@ const createEvacuationCenters = (purgeId) => ({
   purge_id: purgeId,
 });
 
+//Incident Log Generator
+const createLog = (purgeId) => ({
+  log_id: faker.number.int(),
+  sender_name: faker.person.fullName(),
+  sender_id: faker.string.uuid(),
+  sender_type: faker.helpers.arrayElement(["police", "civilian"]),
+  time: faker.time.recent(),
+  date: faker.date.anytime().toISOString().split("T")[0],
+  log_message: faker.lorem.sentence(),
+  language: faker.helpers.arrayElement(["en"]),
+  incident_id: faker.string.uuid(),
+  purge_id: purgeId,
+});
 
+//Incident FR chat Generator
+const createFrsChat = (purgeId) => ({
+  frschat_id: faker.number.int(),
+  name: faker.person.fullName(),
+  message: faker.lorem.sentence(),
+  time: faker.time.recent(),
+  date: faker.date.anytime().toISOString().split("T")[0],
+  responder_type: faker.helpers.arrayElement(["Fire fighter", "EMS", "police"]),
+  responder_id: faker.string.uuid(),
+  images: JSON.stringify([faker.image.url()]), // Example image URLs as JSON
+  video: JSON.stringify([faker.image.url()]), // Example video URLs as JSON
+  message_type: faker.helpers.arrayElement(["sender", "reciever"]),
+  language: faker.helpers.arrayElement(["en"]),
+  incident_id: faker.number.int(),
+  purge_id: purgeId,
+});
 
+//Incident Civilian chat Generator
+const createCivilianChat = (purgeId) => ({
+  civilianchat_id: faker.number.int(),
+  name: faker.person.fullName(),
+  sender_type: faker.helpers.arrayElement(["civilian", "witness"]),
+  message: faker.lorem.sentence(),
+  time: faker.time.recent(),
+  date: faker.date.anytime().toISOString().split("T")[0],
+  id: faker.string.uuid(),
+  images: JSON.stringify([faker.image.url()]), // Example image URLs as JSON
+  videos: JSON.stringify([faker.image.url()]), // Example video URLs as JSON
+  message_type: faker.helpers.arrayElement(["sender", "reciever"]),
+  language: faker.helpers.arrayElement(["en"]),
+  incident_id: faker.number.int(),
+  purge_id: purgeId,
+});
+
+////Incident Generator
+const createIncident = (purgeId) => ({
+  incident_id: faker.number.int(),
+  incident_name: faker.lorem.words(3),
+  incident_type: JSON.stringify({
+    name: faker.helpers.arrayElement([
+      "FRW-Fire Warning",
+      "Earthquake Warning",
+      "Child-Abduction",
+      "accident",
+    ]),
+    type: faker.helpers.arrayElement([
+      "Natural Disaster",
+      "Missing Persons",
+      "Public Health Emergencies",
+      "Others",
+    ]),
+  }),
+  incident_coordinates: {
+    coordinates: [faker.location.latitude(), faker.location.longitude()],
+  },
+  incident_affected_area: JSON.stringify({
+    radius: faker.number.float({ min: 0.5, max: 10, precision: 0.1 }),
+    unit: "km",
+  }),
+  location: faker.location.streetAddress(),
+  issuing_authority: faker.person.fullName(),
+  description: faker.lorem.paragraph(),
+  status: faker.helpers.arrayElement([
+    "active",
+    "inactive",
+    "under investigation",
+  ]),
+  officerIncharge: faker.person.fullName(),
+  severity: faker.helpers.arrayElement(["low", "moderate", "high", "critical"]),
+  latest_report: faker.lorem.sentence(),
+  date: faker.date.anytime().toISOString().split("T")[0],
+  time: faker.time.recent(),
+  incidentInformation: JSON.stringify({
+    report_id: faker.string.uuid(),
+    details: faker.lorem.sentences(2),
+  }),
+  language: faker.helpers.arrayElement(["en"]),
+  cityname: faker.location.city(),
+  purge_id: purgeId,
+});
 
 export {
   createCameraData,
   createFirstResponderData,
   createCMSData,
- createEvacuationCenters
+  createEvacuationCenters,
+  createLog,
+  createIncident,
+  createCivilianChat,
+  createFrsChat,
 };
